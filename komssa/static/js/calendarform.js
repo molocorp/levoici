5fcc251150d7844702320d97a5a487b254947238
Martial <martial.nodem@auf.org>
2015-09-16 11:52:39 +0100

    un annuaire tout simple

---
+++ komssa/static/js/calendarform.js
@@ -0,0 +1,16 @@
+ $('.dateanniv').datepicker({
+                clearBtn: true,
+                language: "fr"
+        });
+        
+ $('.dp1').each(function(index){
+
+             $(this).datepicker({
+                    todayBtn: "linked",
+                    clearBtn: true,
+                    language: "fr",
+                    orientation: "top left"
+            });
+
+}); 
+        
